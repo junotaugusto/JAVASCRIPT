bd39ee84e1a83345b7def21125a1577c2fb50749
Junot Augusto Patrial Cordeiro <junotpcordeiro@gmail.com>
2022-10-25 19:56:47 -0300

    exercicio

---
+++ se2-aula28/js/exercicio.js
@@ -0,0 +1,20 @@
+        let numero = Number(prompt('Digite um número'));
+        const numeroTitulo = document.getElementById('numero-titulo');
+        const texto = document.getElementById('texto');
+        numero = parseFloat(numero); //Converter para Float para que o número possa aparecer com vírgulas no site.
+
+        numeroTitulo.innerHTML = numero;
+
+        texto.innerHTML = `<p>A raiz quadrada do número escolhido é <strong>${Math.sqrt(numero).toFixed(2)}</strong></p>`
+        
+        texto.innerHTML += `<p>O tipo do número é ${typeof(numero)}</p>`
+        
+        texto.innerHTML += (`<p>O número ${numero} é inteiro? <strong>${Number.isInteger(numero)} </p>`);
+
+        texto.innerHTML += (`<p> O número ${numero} não é um número (NaN)? ${Number.isNaN(numero)}</p>`);
+
+        texto.innerHTML += (`<p>O número ${numero}, arredondado para cima, fica ${Math.ceil(numero)}</p>`);
+
+        texto.innerHTML += (`<p>O número ${numero}, arredondado para baixo fica ${Math.floor(numero)}</p>`);
+
+        
