4dfac9328cba5f8aa3a2d5418d14cb9f8ef021ac
Junot Augusto Patrial Cordeiro <junotpcordeiro@gmail.com>
2022-10-29 10:12:16 -0300

    Update index.js

--- se2-aula31/index.js
+++ se2-aula31/index.js
@@ -1,6 +1,7 @@
 //SEÇÃO 2 - AULA 31 - FUNÇÕES(BÁSICO).
 //OBS: Para ler os cógidos, os retire dos comentários /* */
 
+// CRIANDO UMA FUNÇÃO:
 // Uma função é uma linha de código que executa uma ação.
 // Geralmente as funções servem para retornar um valor referente ao valor inserido anteriormente.
 // Para uma função ser criada em JavaScript, utilize a palavra "function".
@@ -39,7 +40,7 @@ console.log(`A constante "variavel" recebeu a função "saudacao" que retornou a
 
 // Então, vimos que as funções recebem um valor, pode executar uma ação, pode ou não também retornar um valor. Existem funções que não irão retornar um valor, mas, por enquanto, estamos vendo funções que retornam valores. 
 
-// Criando uma função que faz a soma entre dois valores:
+// CRIANDO UMA FUNÇÃO COM MAIS DE UM ARGUMENTO:
 
 function soma(x, y) {
     const resultado = x+y;
@@ -57,7 +58,8 @@ resultado = soma(100+100); // Isso não pode ser feito
 console.log(resultado); //Isso trará um erro.
 */
 
-// Atribuindo valores padrão para as funções:
+// ATRIBUINDO VALORES PADRÃO NAS FUNÇÕES:
+
 function soma2(x = 10, y = 20) {
     const resultado2 = x + y;
     return resultado2
@@ -65,6 +67,8 @@ function soma2(x = 10, y = 20) {
 console.log(soma2(3, 3)); //Utilizando esses argumentos, a conta será feita normalmente e o resultado será 6, neste caso.
 console.log(soma2()); //Porém, se não inserirmos nenhum argumento, ele executará os argumentos padrão, os valores que foram definidos previamente na função que, neste caso, resulta em 30.
 console.log(soma2(5));  //Lembrando que, no caso de apenas um argumento for inserido, ele utilizará o valor inserido e o valor padrão do outro argumento. Neste caso, o x é igual a 5 e o y não foi definido, porém possui o valor de 20 como padrão, resultando em 25 a soma de ambos.
+
+//CRIANDO UMA FUNÇÃO ANÔNIMA (AQUELAS QUE ESTÃO DENTRO DE VARIÁVEIS)
  
 
 
