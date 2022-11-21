4687b7ddec9687a7a0385c0b9dfe706512061aff
Junot Augusto Patrial Cordeiro <junotpcordeiro@gmail.com>
2022-11-21 14:09:09 -0300

    Update script.js

--- se3-aula41/script.js
+++ se3-aula41/script.js
@@ -1,1 +1,8 @@
 //SEÇÃO 3 - AULA 40 - IF, ELSE IF E ELSE - PARTE 2
+const numero = 11;
+
+if(numero >=0 && numero <=5){
+    console.log('O número está entre 0 e 5')
+} else if (numero >= 10 && numero <= 15){
+    console.log('O número está entre 10 e 15.')
+}
