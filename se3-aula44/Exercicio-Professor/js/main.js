e9171180b899585a13f10c9dca3396100282be3e
Junot Augusto Patrial Cordeiro <junotpcordeiro@gmail.com>
2022-11-26 09:37:01 -0300

    ......

--- se3-aula44/Exercicio-Professor/js/main.js
+++ se3-aula44/Exercicio-Professor/js/main.js
@@ -9,7 +9,7 @@ form.addEventListener('submit', function (e) { //e de evento. O elemento aqui é
  const altura = Number(inputAltura.value)
 
  if(!peso) { //Se o peso retornar um falsy, NaN etc...
-        setResultado('Peso Inválido', false)
+        setResultado('Peso Inválido', false) //False aqui responde ao "isValid" da função setResultado
         return; //Para de executar o código e volta.
  }
  if(!altura) {
@@ -18,11 +18,35 @@ form.addEventListener('submit', function (e) { //e de evento. O elemento aqui é
  }
 
  const imc = getIMC(peso, altura)
- console.log(imc)
+ const nivelIMC = getNivelIMC(imc)
+ const msg = `Seu Índice de Massa Corporal é de ${imc}. ${nivelIMC}`;
+ setResultado(msg, true); //True aqui responde ao "isValid" da função setResultado
+ }) 
+ //A função está dentro do Listener que é quem "executa" a função dentro do elemento. Pelo que eu entendi, ele coloca o listener no javascript ao invés de inserir a função diretamente na tag do HTML.
 
- }
-) //A função está dentro do Listener que é quem "executa" a função dentro do elemento. Pelo que eu entendi, ele coloca o listener no javascript ao invés de inserir a função diretamente na tag do HTML.
+function getNivelIMC(imc) {
+        const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade 1', 'Obesidade 2', 'Obesidade 3', 'Pare de comer e procure um médico AGORA']
+        if(imc > 50) return nivel[6]
+        if(imc >= 39.9) return nivel[5] 
+        
+        if(imc >= 34.9) {
+                return nivel[4]
+        } 
+        if(imc >= 29.9) {
+                return nivel[3]
+        } 
+        if(imc >= 24.9) {
+                return nivel[2]
+        } 
+        if (imc >= 18.5) {
+                return nivel[1]
+        } 
+        if (imc < 18.5){
+                return nivel[0]
+        }
+}
 
+//Sempre que na função a gente acha a palavra return, a função para ali e não executa o restante. Por isso, o else em else if na função acima não é necessário. Se ele encontra e retorna o que deve retornar, a função ja para. Outra coisa interessante é que, se temos apenas uma linha no código do if, não é necessário o uso de chaves, conforme a primeira linha de código acima. A checagem foi feita de trás para frente justamente por causa de return.
 
 function getIMC(peso, altura){      
         const imc = peso / altura ** 2
@@ -38,6 +62,11 @@ function setResultado(msg, isvalid){
         const resultado = document.querySelector('#resultado')
         resultado.innerHTML = '';
         const p = criaP()
+        if (isvalid) { 
+                p.classList.add('paragrafo-resultado')
+        } else {
+                p.classList.add('bad')
+        }
         p.innerHTML = msg
         resultado.appendChild(p) //insere o elemento como filho.
 }
