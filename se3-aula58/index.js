641160792542c75f62e4a0e1493390b513fed596
Junot Augusto Patrial Cordeiro <junotpcordeiro@gmail.com>
2022-12-08 07:07:07 -0300

    Create index.js

---
+++ se3-aula58/index.js
@@ -0,0 +1,25 @@
+//SEÇÃO 3 - AULA 58 - WHILE, DO WHILE - ESTRUTURAS DE REPETIÇÃO.
+
+//Geralmente, no loop for nós sabemos o tamanho da estrutura de repetição. Do length. Com while, é diferente, como o próprio nome propõe.
+
+// let i = 0; //variável criada fora do while
+// while(i <= 10){
+//     console.log(i)
+//     i++
+// }
+
+function random(min, max){
+    const r = Math.random() * (max - min) + min;
+    return Math.floor(r)
+}
+const min = 1
+const max = 20
+let rand = random(min, max);
+contador = 0
+
+while(rand !== 10){
+    rand = random(min, max)
+    contador = contador + 1
+}
+
+console.log(contador)
