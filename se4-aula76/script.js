9f7ef82003963902def111076ed1638099c1e211
Junot Augusto Patrial Cordeiro <junotpcordeiro@gmail.com>
2023-02-06 11:41:08 -0300

    .

--- se4-aula76/script.js
+++ se4-aula76/script.js
@@ -1,3 +1,31 @@
 //SEÇÃO 4 - AULA 76 - PRATICANDO COM FACTORY FUNCTIONS
 
 // NESTA AULA FIZEMOS EXERCÍCIOS QUE ESTÃO NOS ARQUIVOS MODELO.
+
+function criaCalculadora() { //Factory Function que cria a calculadora
+    return { //Retorna um objeto
+        display: document.querySelector(".display"),
+
+
+        inicia() {
+            this.cliqueBotoes()
+        },  
+        cliqueBotoes(){
+            document.addEventListener('click', function(e) {
+                const el = e.target;
+                console.log(this)
+
+                if(el.classList.contains('btn-num')) {
+                    this.btnParaDisplay();                
+                }
+            });
+        }, 
+
+        btnParaDisplay() {
+
+        }
+    };
+} 
+
+const calculadora = criaCalculadora()
+calculadora.inicia();
