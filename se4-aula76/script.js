6c4e392a1b0c21c08fee2124cade71d91b3b40f2
Junot Augusto Patrial Cordeiro <junotpcordeiro@gmail.com>
2023-02-10 18:53:48 -0300

    Update script.js

--- se4-aula76/script.js
+++ se4-aula76/script.js
@@ -5,27 +5,45 @@
 function criaCalculadora() { //Factory Function que cria a calculadora
     return { //Retorna um objeto
         display: document.querySelector(".display"),
+        btnClear: document.querySelector(".btn-clear"),
 
+        clearDisplay() {
+            this.display.value = ''
+        },
 
         inicia() {
             this.cliqueBotoes()
         },  
+        deleteUm(){
+            this.display.value = this.display.value.slice(0, -1);
+        },
+        realizaConta(){
+
+        },
         cliqueBotoes(){
             document.addEventListener('click', function(e) {
                 const el = e.target;
-                console.log(this)
 
                 if(el.classList.contains('btn-num')) {
-                    this.btnParaDisplay();                
+                    this.btnParaDisplay(el.innerText);                
                 }
-            });
+                if(el.classList.contains('btn-clear')){
+                    this.clearDisplay()
+                }
+                if(el.classList.contains('btn-del')){
+                    this.deleteUm()
+                }
+                if(el.classList.contains('btn-eq')){
+                    this.realizaConta()
+                }
+            }.bind(this));
         }, 
 
-        btnParaDisplay() {
-
+        btnParaDisplay(valor) {
+            this.display.value += valor
         }
     };
 } 
 
 const calculadora = criaCalculadora()
-calculadora.inicia();
+calculadora.inicia(); 
